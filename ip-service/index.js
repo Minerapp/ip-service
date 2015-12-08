2253d7105ab5a07d09efb6a2ec072f5f10e27d48
miner-ops <miner-tech@minerapp.com>
2015-12-08 16:03:40 -0500

    Update index.js

--- ip-service/index.js
+++ ip-service/index.js
@@ -1,7 +1,7 @@
 var hostname = require('os').hostname()
 var ip = require('ip').address()
 console.log(process.env.PORT || 4100)
-require('http').createServer(function (err, res) {
+require('http').createServer(function (req, res) {
   res.write(JSON.stringify({
     ip: ip
   }));
